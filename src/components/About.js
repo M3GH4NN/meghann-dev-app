cb0259cbef691a849f17c5b52e94e2dd4b7ba9f5
Meghann Cunningham <mc@meghann.dev>
2021-05-21 19:56:50 -0400

    fixed image size and added pic of dogs

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,15 @@ return (
     <div>
 <section id='about'>
 <div className='row'>
+
 <div className='three columns'>
 <img className='profile-pic' src='images/Snapchat-435441263.jpg' alt='Meghann Cunningham' />
+<img className='dog-pic' src='images/dogs.jpeg' alt='Tan mini poodle and black cockapoo puppy' />
+
 </div>
+
 <div className='nine columns main-col'>
+
 <h2>About Me</h2>
 <p>
 {
