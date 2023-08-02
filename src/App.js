0db0e623ba743f20fcc32f0177c879e7d8d9c79b
Meghann Cunningham <mc@meghann.dev>
2023-08-01 22:47:18 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -7,17 +7,38 @@ import ContactUs from "./components/ContactUs";
 import Footer from "./components/Footer";
 import resumeData from "./components/resumeData";
 import GameDemo from "./components/Game";
-class App extends Component {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         {/* <Header resumeData={resumeData} /> */}
+//         {/* <About resumeData={resumeData} /> */}
+//         {/* <Resume resumeData={resumeData} /> */}
+//         {/* <Portfolio resumeData={resumeData} /> */}
+//         {/* <ContactUs resumeData={resumeData} /> */}
+//         {/* <Footer resumeData={resumeData} /> */}
+//         <GameDemo />
+//       </div>
+//     );
+//   }
+// }
+
+var demo = "/game/demo.html";
+
+class App extends React.Component {
   render() {
     return (
-      <div>
-        {/* <Header resumeData={resumeData} /> */}
-        {/* <About resumeData={resumeData} /> */}
-        {/* <Resume resumeData={resumeData} /> */}
-        {/* <Portfolio resumeData={resumeData} /> */}
-        {/* <ContactUs resumeData={resumeData} /> */}
-        {/* <Footer resumeData={resumeData} /> */}
-        <GameDemo />
+      <div style={{ display: "unset", margin: "unset" }}>
+        <iframe
+          title="Hex of the Turtle Islands Game"
+          style={{
+            width: "100vw",
+            height: "100vh",
+            border: "unset",
+            position: "fixed",
+          }}
+          src={demo}
+        ></iframe>
       </div>
     );
   }
