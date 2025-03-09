899283e931a55721174ca14a3c1c9349dbfe390f
Meghann Cunningham <mc@meghann.dev>
2025-03-09 13:29:59 -0500

    updated resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 export default class Resume extends Component {
   render() {
     let resumeData = this.props.resumeData;
@@ -12,25 +13,23 @@ export default class Resume extends Component {
           </div>
           <div className="nine columns main-col">
             {resumeData.education &&
-              resumeData.education.map((item) => {
-                return (
-                  <div className="row item">
-                    <div className="twelve columns">
-                      <h3>{item.UniversityName}</h3>
-                      <p className="info">
-                        {item.specialization}
-                        <span>&bull;</span>{" "}
-                        <em className="date">
-                          {item.MonthOfPassing} {item.YearOfPassing}
-                        </em>
-                      </p>
-                      <p>{item.Achievements}</p>
-                    </div>
+              resumeData.education.map((item, index) => (
+                <div className="row item" key={index}>
+                  <div className="twelve columns">
+                    <h3>{item.UniversityName}</h3>
+                    <p className="info">
+                      {item.specialization}
+                      <span>&bull;</span>{" "}
+                      <em className="date">
+                        {item.MonthOfPassing} {item.YearOfPassing}
+                      </em>
+                    </p>
                   </div>
-                );
-              })}
+                </div>
+              ))}
           </div>
         </div>
+
         <div className="row work">
           <div className="three columns header-col">
             <h1>
@@ -38,195 +37,33 @@ export default class Resume extends Component {
             </h1>
           </div>
           <div className="nine columns main-col">
-            {/* {resumeData.work &&
-              resumeData.work.map((item) => {
-                console.log(item.Achievements[0]);
-                return (
-                  <div className="row item">
-                    <div className="twelve columns">
-                      <h3>{item.CompanyName}</h3>
-                      <p className="info">
-                        {item.specialization}
-                        <span>&bull;</span>{" "}
-                        <em className="date">
-                          {item.MonthOfLeaving} {item.YearOfLeaving}
-                        </em>
-                      </p>
-                      <p>
-                        <ul>
-                          <li>
-                            <span>&bull;</span> {item.Achievements[0]}
+            {resumeData.work.map((item, index) => (
+              <div className="row item" key={index}>
+                <div className="twelve columns">
+                  <h3>{item.CompanyName}</h3>
+                  <p className="info">
+                    {item.specialization}
+                    <span>&bull;</span>{" "}
+                    <em className="date">
+                      {item.MonthOfLeaving} {item.YearOfLeaving}
+                    </em>
+                  </p>
+                  <ul>
+                    {item.Achievements &&
+                      item.Achievements.filter((a) => a && a.trim() !== "").map(
+                        (achievement, i) => (
+                          <li key={i}>
+                            <span>&bull;</span> {achievement}
                           </li>
-                          <li>
-                            <span>&bull;</span> {item.Achievements[1]}
-                          </li>
-                          <li>
-                            <span>&bull;</span> {item.Achievements[2]}
-                          </li><li>
-                            <span>&bull;</span> {item.Achievements[3]}
-                          </li>
-                        </ul>
-                        <ul></ul>
-                      </p>
-                    </div>
-                  </div>
-                );
-              })} */}
-            {/* {resumeData.work[0] && */}
-            {/* resumeData.work.map((item) => { */}
-            {/* // console.log(item.Achievements[0]); */}
-            {/* return ( */}
-            <div className="row item">
-              <div className="twelve columns">
-                <h3>{resumeData.work[0].CompanyName}</h3>
-                <p className="info">
-                  {resumeData.work[0].specialization}
-                  <span>&bull;</span>{" "}
-                  <em className="date">
-                    {resumeData.work[0].MonthOfLeaving}{" "}
-                    {resumeData.work[0].YearOfLeaving}
-                  </em>
-                </p>
-                <p>
-                  <ul>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[0].Achievements[0]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[0].Achievements[1]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[0].Achievements[2]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[0].Achievements[3]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[0].Achievements[4]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[0].Achievements[5]}
-                    </li>
+                        )
+                      )}
                   </ul>
-                </p>
+                </div>
               </div>
-            </div>
-
-            <div className="row item">
-              <div className="twelve columns">
-                <h3>{resumeData.work[1].CompanyName}</h3>
-                <p className="info">
-                  {resumeData.work[1].specialization}
-                  <span>&bull;</span>{" "}
-                  <em className="date">
-                    {resumeData.work[1].MonthOfLeaving}{" "}
-                    {resumeData.work[1].YearOfLeaving}
-                  </em>
-                </p>
-                <p>
-                  <ul>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[0]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[1]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[2]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[3]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[4]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[5]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[6]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[7]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[8]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[9]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[10]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[1].Achievements[11]}
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className="row item">
-              <div className="twelve columns">
-                <h3>{resumeData.work[2].CompanyName}</h3>
-                <p className="info">
-                  {resumeData.work[2].specialization}
-                  <span>&bull;</span>{" "}
-                  <em className="date">
-                    {resumeData.work[2].MonthOfLeaving}{" "}
-                    {resumeData.work[2].YearOfLeaving}
-                  </em>
-                </p>
-                <p>
-                  <ul>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[2].Achievements[0]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[2].Achievements[1]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[2].Achievements[2]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[2].Achievements[3]}
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className="row item">
-              <div className="twelve columns">
-                <h3>{resumeData.work[3].CompanyName}</h3>
-                <p className="info">
-                  {resumeData.work[3].specialization}
-                  <span>&bull;</span>{" "}
-                  <em className="date">
-                    {resumeData.work[3].MonthOfLeaving}{" "}
-                    {resumeData.work[3].YearOfLeaving}
-                  </em>
-                </p>
-                <p>
-                  <ul>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[3].Achievements[0]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[3].Achievements[1]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[3].Achievements[2]}
-                    </li>
-                    <li>
-                      <span>&bull;</span> {resumeData.work[3].Achievements[3]}
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
+
         <div className="row skill">
           <div className="three columns header-col">
             <h1>
@@ -234,23 +71,16 @@ export default class Resume extends Component {
             </h1>
           </div>
           <div className="nine columns main-col">
-            <p>{resumeData.skillsDescription}</p>
-            {/* <div className='bars'> */}
             <ul className="skills">
               {resumeData.skills &&
-                resumeData.skills.map((item) => {
-                  return (
-                    <li>
-                      {/* <span className={`bar-expand ${item.skillname.toLowerCase()}`}> */}
-                      {/* </span> */}
-                      <em>{item.skillname}</em>
-                    </li>
-                  );
-                })}
+                resumeData.skills.map((item, index) => (
+                  <li key={index}>
+                    <em>{item.skillname}</em>
+                  </li>
+                ))}
             </ul>
           </div>
         </div>
-        {/* </div> */}
       </section>
     );
   }
--- src/components/resumeData.js
+++ src/components/resumeData.js
@@ -47,56 +47,40 @@ let resumeData = {
         "Optimize ECS-based services, integrating geolocation-based emergency response capabilities to enhance accessibility and reliability.",
         "Collaborate with cross-functional teams, ensuring seamless DevOps support for applications serving the Deaf and Hard of Hearing community.",
         "Reduce infrastructure costs and downtime by optimizing cloud automation and task scheduling.",
-        "Reduce AWS costs by conducting deep dive analyses of AWS usage, identifying and decommissioning outdated infrastructure, and optimizing cloud resource allocation, resulting in significant cost savings."
       ],
     },
-
     {
-      CompanyName: "EITR Technolgies",
+      CompanyName: "EITR Technologies",
       specialization: "Platform Engineer",
       MonthOfLeaving: "December",
       YearOfLeaving: "2022 to May 2024",
       Achievements: [
-        "Develop and implement scalable, reliable, and efficient platform solutions based on business requirements and industry best practices",
-        "Automate infrastructure provisioning, configuration, and deployment processes to ensure consistency and repeatability",
-        "Establish and maintain DevSecOps practices",
-        "Implement monitoring, logging, and alerting systems to proactively identify and resolve performance bottlenecks, scalability issues, and security vulnerabilities",
-        "Ensure platform security by implementing appropriate security measures, adhering to industry standards, and conducting regular security audits and asseessments",
-        "Tools and languages utilized: Python, Terraform, Terragrunt, Salt Stack, Docker, Golang, GitLab CI/CD, AWS CLI, AWS IAM, AWS S3, AWS EC2, AWS VPC, AWS DynamoDB, AWS Route53, AWS Certificate Manager, AWS Lambda, AWS CloudWatch, AWS ECS, AWS ECR, GCP, Azure"
+        "Develop and implement scalable, reliable, and efficient platform solutions.",
+        "Automate infrastructure provisioning, configuration, and deployment processes.",
+        "Establish and maintain DevSecOps practices.",
+        "Implement monitoring, logging, and alerting systems.",
+        "Ensure platform security by implementing appropriate security measures.",
       ],
     },
-
     {
       CompanyName: "Applied Insight (formerly Stratus Solutions)",
       specialization: "Game Dev Team Lead & DevOps Engineer",
       MonthOfLeaving: "September",
       YearOfLeaving: "2019 to December 2022",
       Achievements: [
-        "Promoted to Game Development Team Lead in 2022",
-        "Moved to a Game Development project in 2021 as the sole DevOps engineer in charge of the game's AWS infrastructure and website",
-        "Responsible for making sure the website and game were compliant with NSA security standards and policies",
-        "Built custom front-end and back-end components for game and website integration",
-        "Made sure network security was compliant",
-        "Ensured documentation for the game and website were accurate, detailed, and up to date",
-        "Maintained, automated, and improved the GitLab pipelines",
-        "Helped with game design and development",
-        "Worked on a development team responsible for developing AWS automation tools for a classified, deployable infrastructure platform",
-        "Created tools for our team to help with governance of our customers’ cloud accounts",
-        "Account management, budget enforcement, and compliance automation",
-        "Tools and languages utilized: React, Javascript, Node, Express, CSS, HTML, Python, Golang, JSON, YAML, Terraform, GitLab CI/CD, AWS CLI, AWS Amplify, AWS Cognito, AWS PinPoint, AWS Kinesis, AWS S3, AWS EC2, AWS VPC, AWS API, AWS DynamoDB, AWS Route53, AWS Certificate Manager, AWS Cost Explorer, AWS CloudFront, AWS CloudFormation, AWS Lambda, AWS CloudWatch, AWS SQS, AWS SMS, AWS ECS, AWS ECR, AWS GuardDuty",
+        "Promoted to Game Development Team Lead in 2022.",
+        "Managed AWS infrastructure and website for a game development project.",
+        "Ensured NSA security compliance for game and website.",
       ],
     },
-
     {
       CompanyName: "Stratus Solutions",
       specialization: "DevOps Engineer Intern",
       MonthOfLeaving: "June",
       YearOfLeaving: "2019 to August 2019",
       Achievements: [
-        "Created networking automation tools within AWS for a classified, deployable infrastructure platform",
-        "The tools were immediately deployed into the project’s classified production environment",
-        "Gave a presentation to the entire company on the internship project",
-        "Tools and languages utilized: Python, Golang, JSON, YAML, AWS CLI, AWS CloudFormation, AWS Lambda, AWS EC2, AWS VPC, AWS API Gateway, AWS DynamoDB, AWS Transit Gateway, AWS S3, AWS CloudWatch, AWS SQS, AWS SMS, ",
+        "Created networking automation tools within AWS.",
+        "Presented internship project to the entire company.",
       ],
     },
     {
@@ -105,64 +89,21 @@ let resumeData = {
       MonthOfLeaving: "January",
       YearOfLeaving: "2013 to September 2019",
       Achievements: [
-        "Intake, assessment, and fulfillment of sign language interpreter requests",
-        "Maintained and improved internal customer and interpreter database",
-        "Assisted with business development",
-        "Knowledgeable in the ADA and HIPAA",
+        "Handled interpreter requests and scheduling.",
+        "Managed internal customer and interpreter database.",
+        "Assisted with business development and compliance (ADA, HIPAA).",
       ],
     },
   ],
-  skillsDescription: "",
   skills: [
     {
-      skillname:
-        "Languages: React, JavaScript, Node, Python, Express, HTML, CSS, NoSQL, Bash",
+      skillname: "React, JavaScript, Node, Python, Express, HTML, CSS, Bash",
     },
     {
       skillname:
-        "AWS: Amplify, API Gateway, CLI, CloudFormation, CloudTrail, CloudWatch, Cognito, DynamoDB, EC2, ECR, ECS, ELB, IAM, Lambda, Pinpoint, Route53, S3, SNS, SQS, Transit Gateway, VPC   ",
-    },
-    {
-      skillname:
-        "Tools/Software: CMD, Confluence, GitLab, Jira, Kion/Cloudtamer, PowerShell, Terraform, Terragrunt",
-    },
-    {
-      skillname: "Platforms: AWS, Azure, Docker, GCP, Linux, Windows",
-    },
-  ],
-  //   TODO: add pictures of personal projects so that the box isn't empty...
-  // update hypetrain picture and gitlab to show current version of the site
-  // make site live????
-  portfolio: [
-    {
-      name: "HypeTrain",
-      description:
-        "I created a Python program that uses HTTP Requests and APIs to grab data from a public video game database in order to create a list of upcoming video game release dates. The user creates a list by searching for a game that they would like to keep track of and adding that game to their Watch List. The list can be modified at any time by the user. The user can also click an auto-update button that runs a function that will go through their watch list, remove any game whose release date has passed, and update any release dates that have changed. I am also working on transforming this program into a website (MERN Stack).",
-      imgurl: "images/dualsense.jpg",
-      url: "https://github.com/M3GH4NN/hypetrain",
-    },
-    {
-      name: "Custom Smart Home Skills",
-      description:
-        "I have created several smart home skills to help deaf family members. One example is that I linked my Ring doorbell to my Echo devices and then added custom logic to flash the smart lights in the house if someone rings the doorbell. The flashing smart lights will alert my mother that someone is at the door.",
-      imgurl: "images/smarthome.jpg",
-    },
-    {
-      name: "Smart Monitor",
-      description:
-        "I created a smart monitor using my Raspberry Pi and a spare monitor. The monitor displays my calendar for the next four weeks, the current weather, the forecast for the rest of the week, and has a rotating background that uses images from a Google Photos smart album of my dogs.",
-      imgurl: "images/20220418_090624.jpg",
-    },
-  ],
-  testimonials: [
-    {
-      description: "This is a sample testimonial",
-      name: "Some technical guy",
-    },
-    {
-      description: "This is a sample testimonial",
-      name: "Some technical guy",
+        "AWS: Amplify, API Gateway, CLI, CloudFormation, CloudWatch, Cognito, DynamoDB, EC2, ECS, IAM, Lambda, S3, SNS, SQS, VPC",
     },
   ],
 };
+
 export default resumeData;
